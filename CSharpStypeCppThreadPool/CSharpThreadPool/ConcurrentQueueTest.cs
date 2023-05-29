775ea362c4c3b1fec2ea7ecdb08e8c6454e96fd7
NoemieGrf <epigrf@gmail.com>
2023-05-29 19:43:42 +0800

    update

---
+++ CSharpStypeCppThreadPool/CSharpThreadPool/ConcurrentQueueTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace CSharpThreadPool
+{
+    internal class ConcurrentQueueTest
+    {
+        ConcurrentQueue<int> _concurrentQueue = new ConcurrentQueue<int>();
+
+        public void Run()
+        {
+            Thread thread1 = new Thread(ThreadWrite1);
+            Thread thread2 = new Thread(ThreadWrite2);
+            Thread threadRead = new Thread(ThreadRead);
+
+            thread1.Start();
+            thread2.Start();
+            threadRead.Start();
+
+            thread1.Join();
+            thread2.Join();
+            threadRead.Join();
+        }
+
+        private void ThreadWrite1()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                _concurrentQueue.Enqueue(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void ThreadWrite2()
+        {
+            for (int i = 101; i < 200; ++i)
+            {
+                _concurrentQueue.Enqueue(i);
+                Thread.Sleep(50);
+            }
+        }
+
+        private void ThreadRead()
+        {
+            while (_concurrentQueue.Count > 0)
+            {
+                if (_concurrentQueue.TryDequeue(out var number))
+                {
+                    Console.WriteLine(number);
+                    Thread.Sleep(150);
+                }
+            }
+            
+        }
+    }
+}
--- CSharpStypeCppThreadPool/CSharpThreadPool/Program.cs
+++ CSharpStypeCppThreadPool/CSharpThreadPool/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CSharpThreadPool;
+using System;
+using System.Threading;
+
+namespace ThreadPoolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueueTest concurrentQueueTest = new ConcurrentQueueTest();
+
+            concurrentQueueTest.Run();
+            /*
+            WaitHandle[] waitHandles = new WaitHandle[]
+            {
+                new ManualResetEvent(false),
+                new ManualResetEvent(false)
+            };
+            */
+        }
+
+
+        public static void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+
+    }
+}
