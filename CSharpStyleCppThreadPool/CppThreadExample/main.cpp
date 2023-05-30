fe67f7129c4438b659acccb6b4f705959e526083
NoemieGrf <epigrf@gmail.com>
2023-05-30 18:58:47 +0800

    cpp cond var

--- CSharpStyleCppThreadPool/CppThreadExample/main.cpp
+++ CSharpStyleCppThreadPool/CppThreadExample/main.cpp
@@ -3,12 +3,15 @@
 
 #include "src/example_1_thread_create.h"
 #include "src/example_2_atomic_mutex.h"
+#include "src/example_3_condition_variable.h"
 
 int main()
 {
 	// Example_1::Run();
 
-	Example_2::Run();
+	// Example_2::Run();
+
+	Example_3::Run();
 
 	return 0;
 }
