105a5b7c83f2124c8e1456c3e30ca0f17ba4f28c
NoemieGrf <epigrf@gmail.com>
2023-05-30 19:34:26 +0800

    cpp semaphore, cpp11 & cpp20

--- CSharpStyleCppThreadPool/CppThreadExample/main.cpp
+++ CSharpStyleCppThreadPool/CppThreadExample/main.cpp
@@ -4,6 +4,7 @@
 #include "src/example_1_thread_create.h"
 #include "src/example_2_atomic_mutex.h"
 #include "src/example_3_condition_variable.h"
+#include "src/example_4_semaphore.h"
 
 int main()
 {
@@ -11,7 +12,9 @@ int main()
 
 	// Example_2::Run();
 
-	Example_3::Run();
+	// Example_3::Run();
+
+	Example_4::Run();
 
 	return 0;
 }
