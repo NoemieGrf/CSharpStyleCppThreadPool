1f0c9613b1df8d1730647d86f7cb3a8dbc9b5106
NoemieGrf <epigrf@gmail.com>
2023-06-02 16:58:22 +0800

    concurrent queue with mutex lock

--- CSharpStyleCppThreadPool/CSharpStyleCppThreadPool/src/concurrent_queue.hpp
+++ CSharpStyleCppThreadPool/CSharpStyleCppThreadPool/src/concurrent_queue.hpp
@@ -1,49 +1,61 @@
+#pragma once
 
 #include <thread>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+
 #include <Windows.h>
 
 namespace Grf
 {
-	template<typename T>
-	T LoadConsume(T const* addr)
-	{
-		T v = *const_cast<T const volatile*>(addr);
-		// __memeory_barrier();
-		MemoryBarrier();
-		return v;
-	}
+    template<typename T>
+    class LockConcurrentQueue
+    {
+    public:
+        LockConcurrentQueue()
+        {
+        }
+        
+        void Push(T data)
+        {
+            std::lock_guard lockGuard(_mutex);
+            _queue.push(std::move(data));
+            _condVar.notify_one();
+        }
+        
+        void WaitAndPop(T& value)
+        {
+            std::unique_lock lockGuard(_mutex);
+            _condVar.wait(lockGuard, [this]{
+                return !_queue.empty();
+            });
+            
+            value = std::move(_queue.front());
+            _queue.pop();
+        }
+        
+        bool TryPop(T& value)
+        {
+            std::lock_guard lockGuard(_mutex);
+            if (_queue.empty())
+                return false;
+            
+            value = std::move(_queue.front());
+            _queue.pop();
+            return true;
+        }
+        
+        bool Empty() const
+        {
+            std::lock_guard lockGuard(_mutex);
+            return _queue.empty();
+        }
 
-	template<typename T>
-	void StoreRelease(T* addr, T v)
-	{
-		MemoryBarrier();
-		*const_cast<T volatile*>(addr) = v;
-	}
-
-	size_t const CACHE_LINE_SIZE = 64;
-
-	template<typename T>
-	class ConcurrentQueue
-	{
-	private:	// Struct Define
-		struct Node
-		{
-			Node* next;
-			T value;
-		};
-
-	public:
-
-
-	private:
-		Node* _tail;
-		char _cacheLinePad[CACHE_LINE_SIZE];
-
-		Node* _head;
-		Node* _first;
-		Node* _tailCopy;
-
-
-	};
+    private:
+        mutable std::mutex _mutex;  // mutable for const function
+        std::queue<T> _queue;
+        std::condition_variable _condVar;
+    };
 
 }
